85697d02e3c97f03c2a9e9514bfbd51369a758da
Проскуряков Иван <ivan.proskuryakov.2001@yandex.ru>
2021-12-04 20:09:13 +0300

    Cube-version completed

--- Gl-L3/main.cpp
+++ Gl-L3/main.cpp
@@ -4,19 +4,17 @@
 #include <glm/glm.hpp>
 #include <iostream>
 #include <cmath>
-#include <vector>
+
+bool push_cube_facets = false;
 
 void drawSkeletSphere(GLdouble radius, GLint slices, GLint stacks)
 {
     GLUquadric *shape = gluNewQuadric();
     gluQuadricDrawStyle(shape, GLU_LINE);
-//    gluQuadricDrawStyle(shape, GLU_FILL);
-//    gluQuadricDrawStyle(shape, GLU_POINT);
     gluQuadricNormals(shape, GLU_FLAT);
-//    gluQuadricNormals(shape, GLU_SMOOTH);
     gluSphere(shape, radius, slices, stacks);
 }
-void drawCube(GLfloat size)
+void drawCube(GLfloat size, bool uncurtain = false)
 {
     GLfloat left[] = {
             -size / 2, -size / 2, -size / 2,
@@ -54,6 +52,23 @@ void drawCube(GLfloat size)
             size / 2,  size / 2,  size / 2,
             -size / 2,  size / 2,  size / 2,
     };
+    if(uncurtain){
+        glm::vec3 front_normal(0, 0, - 0.1f);
+        glm::vec3 back_normal(0, 0, 0.1f);
+        glm::vec3 right_normal(0.1f, 0,0);
+        glm::vec3 left_normal(-0.1f, 0,0);
+        glm::vec3 up_normal(0, 0.1f, 0);
+        glm::vec3 bottom_normal(0, -0.1f, 0);
+        for (int i = 0; i < 12; ++i){
+            front[i] += front_normal[i % 3];
+            back[i] += back_normal[i % 3];
+            right[i] += right_normal[i % 3];
+            left[i] += left_normal[i % 3];
+            up[i] += up_normal[i % 3];
+            bottom[i] += bottom_normal[i % 3];
+        }
+    }
+
     GLubyte ind[] = {
             0,1,2,3
     };
@@ -103,7 +118,11 @@ void drawCube(GLfloat size)
         glDrawElements(GL_QUADS, 4, GL_UNSIGNED_BYTE, &ind);
     glDisableClientState(GL_VERTEX_ARRAY);
 }
-
+void checkButton (GLFWwindow* window, int key, int scancode, int action, int mods) {
+    if (key == GLFW_KEY_ENTER && action == GLFW_PRESS){
+        push_cube_facets = ! push_cube_facets;
+    }
+}
 int main(void)
 {
     GLFWwindow* window;
@@ -124,6 +143,10 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
     glEnable(GL_NORMALIZE);
+    glEnable(GL_LIGHTING);
+    glLightModelf(GL_LIGHT_MODEL_TWO_SIDE, GL_TRUE);
+    glEnable(GL_COLOR_MATERIAL);
+    glMateriali(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, 0);
     glEnable(GL_DEPTH_TEST);
     /*Initialize the GLEW library */
     if(glewInit() != GLEW_OK)
@@ -145,23 +168,34 @@ int main(void)
         glfwPollEvents();
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        GLfloat light_diffuse[] = {1,0.5,1};
         if(glfwGetTime() - prev >= 1.0f/20){
             angel += (M_PI / 100.0f);
             prev = glfwGetTime();
         }
+        glEnable(GL_LIGHT0);
+        GLfloat light_pos[3];
+        light_pos[0] = 0.7 * cosf(angel);
+        light_pos[1] = 0.0f;
+        light_pos[2] = 0.7 * sinf(angel);
+        glfwSetKeyCallback(window, checkButton);
         glPushMatrix();
-            glTranslatef(0.7 * cosf(angel), 0, 0.7 * sinf(angel));
+            glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+            GLfloat test[] = {0.0f,0.0f,0.0f, 0.8f};
+            glTranslatef(light_pos[0], light_pos[1], light_pos[2]);
+            glLightfv(GL_LIGHT0, GL_POSITION, test);
             glColor3f(1.0f, 1.0f, 1.0f);
-        drawSkeletSphere(0.1, 20, 20);
+            drawSkeletSphere(0.1, 20, 20);
         glPopMatrix();
 
         glPushMatrix();
             glRotatef(3*M_PI/2.0f + angel * 10, 1,1,1);
-            drawCube(0.5);
+            drawCube(0.5, push_cube_facets);
         glPopMatrix();
 
 
         /* Swap front and back buffers */
+        glDisable(GL_LIGHT0);
         glfwSwapBuffers(window);
     }
     glfwTerminate();
